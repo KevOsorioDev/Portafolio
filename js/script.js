77da7cbbd06f1868d2189c5f1eda361a98c4e243
KevOsorioDev <kevin.osorio.dev@gmail.com>
2024-04-06 00:00:55 -0300

    cambio de toda la estructura de secciones y nuevas animaciones. Pagina en proceso.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
+let header = document.querySelector("#header"),
+    headerLinks = document.querySelectorAll(".header-links"),
+    divQueSeHacer = document.querySelectorAll(".div-que-se-hacer");
 
+window.addEventListener("scroll", function() {
+    let scrollTop = window.pageYOffset;
+
+    if (scrollTop <= 0) {
+      header.classList.remove("scrollAbajo");
+    } 
+    else{
+      header.classList.add("scrollAbajo");
+    }
+  });
+
+divQueSeHacer.forEach(e => {
+    e.addEventListener("mouseover", function() {
+    this.style.width = "40vw"
+});  
+})
+
+divQueSeHacer.forEach(e => {
+  e.addEventListener("mouseout", function() {
+  this.style.width = "8vw"
+  
+}); 
+})
 
 
 // -------------------------------
@@ -12,7 +38,7 @@ function horaActual() {
       mm = (mm < 10) ? "0" + mm : mm;
       // hh = (hh < 12) ? mm + "AM" : mm + "PM";
       if (hh < 12) mm = `${mm} AM`;
-      if (hh > 12) mm = `${mm} PM`;
+      if (hh >= 12) mm = `${mm} PM`;
 
       mm = (mm < 10) ? "0" + mm : mm;
 
